89223890ecd94524d3164b19f31395ef0c058ea0
romaiajr <romaiajr5@gmail.com>
2021-03-26 17:18:46 -0300

    Iniciando tela de login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,40 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
-  </div>
+  <v-container id="login">
+    <v-row>
+      <v-col md="6" offset-md="3"
+        ><v-card id="login-card" elevation="2" class="mx-auto my-12">
+          <v-card-title>
+            <h2>ENTRAR NO AMBIENTE WEB</h2>
+          </v-card-title>
+          <v-form ref="form">
+            <v-text-field label="Usuário" v-model="form.user"></v-text-field>
+            <v-text-field label="Senha" v-model="form.password"></v-text-field>
+            <a><small>Esqueci a senha</small></a>
+            <v-btn block>
+              Login
+            </v-btn></v-form
+          >
+          <h5>Não tem uma conta? <a>Criar Conta</a></h5>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+<script>
+export default {
+  data: () => ({
+    form: { user: "", password: "" },
+  }),
+};
+</script>
+<style scoped>
+#login-card {
+  height: 100% !important;
+  width: 100%;
+}
+#login-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
