a60a861ef840ee081c12a80db593540088b2f00b
Roberto Maia <romaiajr5@gmail.com>
2021-03-31 20:24:09 -0300

    Navbar for Users

---
+++ src/components/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <!-- Navbar -->
+    <v-app-bar color="black accent-4" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Ambiente Web</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <!-- Sidebar -->
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <!-- Sidebar Header User Section -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <!-- Function List -->
+      <v-list nav>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item v-for="(item, idx) in navItems" :key="idx">
+            <v-icon>{{ item.icon }}</v-icon>
+            <v-list-item-title style="margin-left: 8px">{{
+              item.title
+            }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+<script>
+import navlist from "../utils/navlist.json";
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+  computed: {
+    navItems() {
+      return navlist.adm;
+    },
+  },
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
+<style scoped>
+.function-title {
+  margin-left: 8px !important;
+}
+</style>
---
+++ src/views/Management.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-main><Navbar /> </v-main>
+  </v-app>
+</template>
+<script>
+import Navbar from "../components/Navbar";
+export default {
+  components: { Navbar },
+  data: () => ({}),
+};
+</script>
